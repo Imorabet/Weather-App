abc6b7cb43027e2b9bbffc1baf43229ecc6ff266
imorabet <mrabetikhlas700@gmail.com>
2023-01-07 17:08:45 +0100

    edit style n jsx files

--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -13,7 +13,7 @@ export default function Inputs (props){
             className="mt-1  block w-full px-3 py-2 text-slate-800 bg-white border border-slate-300 rounded-md text-md shadow-md placeholder-slate-400 capitalize focus:outline-none focus:border-white focus:ring-1 focus:ring-white"
           />
         <button
-          className="text-md font-medium text-slate-700 absolute inset "
+          className="text-md font-medium text-slate-700 absolute inset"
           type={"submit"}
         >
          <FontAwesomeIcon icon={solid("magnifying-glass")} />
